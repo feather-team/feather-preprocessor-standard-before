7a0df0f25889cb099b641bf6616da412e2e3e8fb
jsyczhanghao <jsyczhanghao@sina.com>
2015-04-21 18:26:52 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if(!feather.config.get('staticMode') && feather.config.get('roadmap.domain') == ''){
-	feather.config.set('roadmap.domain', '<?=$FEATHER_STATIC_DOMAIN?>');
-}
-
 module.exports = function(content, file, conf){
 	['extend-uri'].forEach(function(process){
         content = require('./process/' + process + '.js')(content, file, conf);
